29b4bbe006d791c573d22b52d73f520f6f1eabcc
Gabriel Chavez <gabriel@chavezgu.com>
2014-03-11 13:14:48 +0200

    Join the church of emacs.

---
+++ init.el
@@ -0,0 +1,161 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(load-theme 'solarized-dark t)
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  )
+
+
+
+(setq-default inhibit-startup-screen t)
+
+(menu-bar-mode -1)
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
+(setq-default indent-tabs-mode nil)
+(setq x-select-enable-clipboard t
+        x-select-enable-primary t
+        save-interprogram-paste-before-kill t
+        apropos-do-all t
+        mouse-yank-at-point t
+        save-place-file (concat user-emacs-directory "places")
+        backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                                 "backups"))))
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+
+(when (fboundp 'winner-mode)
+      (winner-mode 1))
+
+(require 'uniquify)
+
+
+(require 'saveplace)
+(setq-default save-place t)
+
+; I copied this from the better defaults 
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
+; USE IT
+(require 'evil)
+(evil-mode 1)
+;;Exit insert mode by pressing j and then j quickly
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+(key-chord-mode 1)
+(evilnc-default-hotkeys)
+
+; Don't use evil with repls
+(evil-set-initial-state 'eshell-mode 'emacs)
+(evil-set-initial-state 'inferior-emacs-lisp-mode 'emacs)
+(evil-set-initial-state 'racket-repl-mode 'emacs)
+
+; GRB: split the windows
+(progn
+  (interactive)
+  (split-window-horizontally 120)
+  (other-window 1)
+  ;(split-window-horizontally 80)
+  ;(other-window 1)
+  (split-window)
+  (other-window 1)
+  (eshell) ()
+  (other-window -3))
+
+; GRB: use C-o and M-o to switch windows
+(global-set-key "\M-o" 'other-window)
+(defun prev-window ()
+  (interactive)
+  (other-window -1))
+(global-set-key "\M-O" 'prev-window)
+
+
+; GRB: highlight trailing whitespace
+(set-default 'show-trailing-whitespace t)
+
+; GRB: open temporary buffers in a dedicated window split
+(setq special-display-regexps
+        '("^\\*Completions\\*$"
+          "^\\*Help\\*$"
+          "^\\*grep\\*$"
+          "^\\*Apropos\\*$"
+          "^\\*elisp macroexpansion\\*$"
+          "^\\*local variables\\*$"
+          "^\\*Compile-Log\\*$"
+          "^\\*Quail Completions\\*$"
+          "^\\*Occur\\*$"
+          "^\\*frequencies\\*$"
+          "^\\*compilation\\*$"
+          "^\\*Locate\\*$"
+          "^\\*Colors\\*$"
+          "^\\*tumme-display-image\\*$"
+          "^\\*SLIME Description\\*$"
+          "^\\*.* output\\*$"           ; tex compilation buffer
+          "^\\*TeX Help\\*$"
+          "^\\*Shell Command Output\\*$"
+          "^\\*Async Shell Command\\*$"
+          "^\\*COMMIT_EDITMSG\\*$" ; Start Magit stuff
+          "^\\**magit: realm*\\*$" ; Start Magit stuff
+          "^\\*Backtrace\\*$"))
+(setq grb-temporary-window (nth 2 (window-list)))
+(defun grb-special-display (buffer &optional data)
+  (let ((window grb-temporary-window))
+    (with-selected-window window
+      (switch-to-buffer buffer)
+      window)))
+(setq special-display-function #'grb-special-display)
+
+(column-number-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(show-paren-mode t)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 120 :width normal)))))
+
+
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
+;; ; Geiser mode hacks
+;; (add-hook 'geiser-mode-hook
+;;           '(lambda ()
+;;              (define-key geiser-mode-map (kbd "C-c d") 'geiser-edit-symbol-at-point)))
+
+(add-hook 'racket-mode-hook
+          '(lambda ()
+             (define-key racket-mode-map (kbd "C-c r") 'racket-run)))
+
+(add-hook 'racket-mode-hook
+          '(lambda ()
+             (define-key racket-mode-map (kbd "C-c t") 'racket-test)))
+
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'racket-mode-hook           #'enable-paredit-mode)
