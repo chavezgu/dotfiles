e04fb8ec5d105189d0895ca1ca1f246540a7df59
Gabriel Alejandro Chavez Gutierrez <gabriel.alejandro.chavez.gutierrez@ericsson.com>
2015-03-17 15:15:56 +0200

    Add xmonad.hs config file.

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,22 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Actions.UpdatePointer
+
+-- The main function.
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+-- Command to launch the bar.
+myBar = "xmobar"
+
+-- Custom PP, configure it as you like. It determines what is being written to the bar.
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+myConfig = defaultConfig
+        { terminal    = "urxvt -e zsh"
+        , modMask     = mod4Mask
+        , borderWidth = 3
+        , logHook = updatePointer (Relative 0.5 0.5)
+        }
