6c69f5b79e853340fde8b1fe73782dd6ee4229c2
Gabriel Alejandro Chavez Gutierrez <gabriel.alejandro.chavez.gutierrez@ericsson.com>
2015-06-29 10:07:40 +0300

    Add init.el
    Seems like we are using emacs again.

--- init.el
+++ init.el
@@ -1,17 +1,14 @@
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
-(load-theme 'solarized-dark t)
-
 (when (>= emacs-major-version 24)
   (require 'package)
   (package-initialize)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
   )
 
-
+(load-theme 'solarized-dark t)
 
 (setq-default inhibit-startup-screen t)
 
-(menu-bar-mode -1)
+;; (menu-bar-mode -1)
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode)
@@ -19,30 +16,33 @@
 
 (setq-default indent-tabs-mode nil)
 (setq x-select-enable-clipboard t
-        x-select-enable-primary t
-        save-interprogram-paste-before-kill t
-        apropos-do-all t
-        mouse-yank-at-point t
-        save-place-file (concat user-emacs-directory "places")
-        backup-directory-alist `(("." . ,(concat user-emacs-directory
-                                                 "backups"))))
+      x-select-enable-primary t
+      save-interprogram-paste-before-kill t
+      apropos-do-all t
+      mouse-yank-at-point t
+      save-place-file (concat user-emacs-directory "places")
+      backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                               "backups"))))
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
-(require 'ido)
-(ido-mode t)
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
 (setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
 
 (when (fboundp 'winner-mode)
-      (winner-mode 1))
+  (winner-mode 1))
 
 (require 'uniquify)
-
 
 (require 'saveplace)
 (setq-default save-place t)
 
-; I copied this from the better defaults 
+;; I copied this from the better defaults
 (global-set-key (kbd "M-/") 'hippie-expand)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
@@ -50,33 +50,24 @@
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 
-; USE IT
+;; Evil mode: Vim rocks.
 (require 'evil)
 (evil-mode 1)
-;;Exit insert mode by pressing j and then j quickly
 (setq key-chord-two-keys-delay 0.5)
 (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
 (key-chord-mode 1)
-(evilnc-default-hotkeys)
+;; Exit insert mode by pressing j and then j quickly
+;; (setq key-chord-two-keys-delay 0.5)
 
-; Don't use evil with repls
+;; Don't use evil with repls
 (evil-set-initial-state 'eshell-mode 'emacs)
-(evil-set-initial-state 'inferior-emacs-lisp-mode 'emacs)
-(evil-set-initial-state 'racket-repl-mode 'emacs)
+(evil-set-initial-state 'shell-mode 'emacs)
+(evil-set-initial-state 'lisp-interaction-mode 'emacs)
+(evil-set-initial-state 'dired-mode 'emacs)
+;; (evil-set-initial-state 'inferior-emacs-lisp-mode 'emacs)
+;; (evil-set-initial-state 'racket-repl-mode 'emacs)
 
-; GRB: split the windows
-(progn
-  (interactive)
-  (split-window-horizontally 120)
-  (other-window 1)
-  ;(split-window-horizontally 80)
-  ;(other-window 1)
-  (split-window)
-  (other-window 1)
-  (eshell) ()
-  (other-window -3))
-
-; GRB: use C-o and M-o to switch windows
+;; GRB: use C-o and M-o to switch windows
 (global-set-key "\M-o" 'other-window)
 (defun prev-window ()
   (interactive)
@@ -84,40 +75,9 @@
 (global-set-key "\M-O" 'prev-window)
 
 
-; GRB: highlight trailing whitespace
+;; GRB: highlight trailing whitespace
 (set-default 'show-trailing-whitespace t)
 
-; GRB: open temporary buffers in a dedicated window split
-(setq special-display-regexps
-        '("^\\*Completions\\*$"
-          "^\\*Help\\*$"
-          "^\\*grep\\*$"
-          "^\\*Apropos\\*$"
-          "^\\*elisp macroexpansion\\*$"
-          "^\\*local variables\\*$"
-          "^\\*Compile-Log\\*$"
-          "^\\*Quail Completions\\*$"
-          "^\\*Occur\\*$"
-          "^\\*frequencies\\*$"
-          "^\\*compilation\\*$"
-          "^\\*Locate\\*$"
-          "^\\*Colors\\*$"
-          "^\\*tumme-display-image\\*$"
-          "^\\*SLIME Description\\*$"
-          "^\\*.* output\\*$"           ; tex compilation buffer
-          "^\\*TeX Help\\*$"
-          "^\\*Shell Command Output\\*$"
-          "^\\*Async Shell Command\\*$"
-          "^\\*COMMIT_EDITMSG\\*$" ; Start Magit stuff
-          "^\\**magit: realm*\\*$" ; Start Magit stuff
-          "^\\*Backtrace\\*$"))
-(setq grb-temporary-window (nth 2 (window-list)))
-(defun grb-special-display (buffer &optional data)
-  (let ((window grb-temporary-window))
-    (with-selected-window window
-      (switch-to-buffer buffer)
-      window)))
-(setq special-display-function #'grb-special-display)
 
 (column-number-mode t)
 (custom-set-variables
@@ -134,33 +94,18 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 120 :width normal)))))
 
+;; Some c-mode stuff
+;; (setq c-default-style "linux")
+(setq c-basic-offset"linux")
 
-(require 'auto-complete)
-(global-auto-complete-mode t)
+;; Snippets
+(add-to-list 'load-path
+             "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas-global-mode 1)
 
-;; ; Geiser mode hacks
-;; (add-hook 'geiser-mode-hook
-;;           '(lambda ()
-;;              (define-key geiser-mode-map (kbd "C-c d") 'geiser-edit-symbol-at-point)))
+;; Autocomplete defaults
+(ac-config-default)
 
-(add-hook 'racket-mode-hook
-          '(lambda ()
-             (define-key racket-mode-map (kbd "C-c r") 'racket-run)))
-
-(add-hook 'racket-mode-hook
-          '(lambda ()
-             (define-key racket-mode-map (kbd "C-c t") 'racket-test)))
-
-(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
-(add-hook 'racket-mode-hook           #'enable-paredit-mode)
-
-;; Default C++ is ugly.
-
-(setq c-default-style "stroustrup"
-      c-basic-offset 4)
+;; Use electric pair
+(electric-pair-mode 1)
