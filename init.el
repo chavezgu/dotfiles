80a05697c8f2dce887aba2725a20ba311826556b
Gabriel Chavez <gabriel@chavezgu.com>
2015-06-30 23:57:52 +0300

    HELM!

--- init.el
+++ init.el
@@ -149,3 +149,10 @@
 ;; Compile
 (global-set-key (kbd "C-x c") 'compile)
 
+;; Helm!
+(require 'helm-config)
+(helm-mode 1)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
