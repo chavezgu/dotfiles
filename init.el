32148c2278e0a412864c0c60a04ea117880a83e8
Gabriel Alejandro Chavez Gutierrez <gabriel.alejandro.chavez.gutierrez@ericsson.com>
2015-07-13 17:08:37 +0300

    Include shell-pop

--- init.el
+++ init.el
@@ -72,12 +72,18 @@
 (set-default 'show-trailing-whitespace t)
 
 
+;; Shell pop
+(require 'shell-pop)
+
 (column-number-mode t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(shell-pop-shell-type (quote ("shell" "*shell*" (lambda nil (eshell)))))
+ '(shell-pop-universal-key "M-`")
+ '(shell-pop-windown-position "bottom")
  '(show-paren-mode t)
  '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
 (custom-set-faces
