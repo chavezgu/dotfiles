24b98fdba8392d5f02a833419caca8a8134c38b9
Gabriel Chavez <gabriel@chavezgu.com>
2015-07-20 12:45:45 +0300

    Switch to ibuffer for C-x C-b

--- init.el
+++ init.el
@@ -46,6 +46,7 @@
 
 ;; I copied this from the better defaults
 (global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "M-z") 'zap-up-to-char)
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
