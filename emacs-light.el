30ea20ad2a7a5f8a5057b28821dac3d416a8b485
Gabriel Alejandro Chavez Gutierrez <gabriel.alejandro.chavez.gutierrez@ericsson.com>
2015-07-21 19:46:11 +0300

    Add emacs-light

---
+++ emacs-light.el
@@ -0,0 +1,57 @@
+(load-theme 'wombat t)
+
+(setq-default inhibit-startup-screen t)
+
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+
+(menu-bar-mode -1)
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
+(autoload 'zap-up-to-char "misc"
+          "Kill up to, but not including ARGth occurrence of CHAR." t)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+(require 'saveplace)
+(setq-default save-place t)
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "M-z") 'zap-up-to-char)
+
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
+(show-paren-mode 1)
+(setq-default indent-tabs-mode nil)
+(setq x-select-enable-clipboard t
+      x-select-enable-primary t
+      save-interprogram-paste-before-kill t
+      apropos-do-all t
+      mouse-yank-at-point t
+      require-final-newline t
+      visible-bell t
+      load-prefer-newer t
+      ediff-window-setup-function 'ediff-setup-windows-plain
+      save-place-file (concat user-emacs-directory "places")
+      backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                               "backups"))))
+
+
+;; GRB: use C-o and M-o to switch windows
+(global-set-key "\M-o" 'other-window)
+(defun prev-window ()
+  (interactive)
+  (other-window -1))
+(global-set-key "\M-O" 'prev-window)
+
+(setq c-default-style "linux")
+
+(electric-pair-mode 1)
