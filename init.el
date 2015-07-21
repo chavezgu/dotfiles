1c711a82c92fe53c80c02299813475a69c5338e4
Gabriel Alejandro Chavez Gutierrez <gabriel.alejandro.chavez.gutierrez@ericsson.com>
2015-07-21 19:45:31 +0300

    Change popshell to shell.

--- init.el
+++ init.el
@@ -87,7 +87,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(shell-pop-shell-type (quote ("shell" "*shell*" (lambda nil (eshell)))))
+ '(shell-pop-shell-type (quote ("shell" "*shell*" (lambda nil (shell)))))
  '(shell-pop-universal-key "M-`")
  '(shell-pop-windown-position "bottom")
  '(show-paren-mode t)
