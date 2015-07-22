9843ad8318b83559d634fbbf4a264c9bdf3445e1
Gabriel Alejandro Chavez Gutierrez <gabriel.alejandro.chavez.gutierrez@ericsson.com>
2015-07-22 15:02:25 +0300

    Add the visible bell.

--- init.el
+++ init.el
@@ -21,6 +21,7 @@
 (setq x-select-enable-clipboard t
       x-select-enable-primary t
       save-interprogram-paste-before-kill t
+      visible-bell t
       apropos-do-all t
       mouse-yank-at-point t
       ediff-window-setup-function 'ediff-setup-windows-plain
