c47509180808953b95c710b7352cb58994d2f671
Gabriel Alejandro Chavez Gutierrez <gabriel.alejandro.chavez.gutierrez@ericsson.com>
2015-07-27 18:49:41 +0300

    Add mobi values.

--- init.el
+++ init.el
@@ -276,3 +276,4 @@
 ;; Set the wanted files.
 (add-to-list 'dired-guess-shell-alist-user '("\\.pdf\\'" "mupdf"))
 (add-to-list 'dired-guess-shell-alist-user '("\\.azw3\\'" "ebook-viewer"))
+(add-to-list 'dired-guess-shell-alist-user '("\\.mobi\\'" "ebook-viewer"))
