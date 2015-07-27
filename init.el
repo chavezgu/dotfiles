059c64bc2be60486855447f6cec4700436eae72c
Gabriel Alejandro Chavez Gutierrez <gabriel.alejandro.chavez.gutierrez@ericsson.com>
2015-07-27 17:05:11 +0300

    Set the ebook-viewer for emacs.

--- init.el
+++ init.el
@@ -274,5 +274,6 @@
 (setq ido-everywhere t)
 (setq ido-enable-flex-matching t)
 
-;; Set mupdf to open pdfs
+;; Set the wanted files.
 (add-to-list 'dired-guess-shell-alist-user '("\\.pdf\\'" "mupdf"))
+(add-to-list 'dired-guess-shell-alist-user '("\\.azw3\\'" "ebook-viewer"))
