69056a25489be6c8099c521b7e29c85037675681
Gabriel Alejandro Chavez Gutierrez <gabriel.alejandro.chavez.gutierrez@ericsson.com>
2015-08-13 13:30:50 +0300

    Set commit verbose as default in magit.

--- init.el
+++ init.el
@@ -123,6 +123,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-commit-arguments (quote ("--verbose")))
  '(shell-pop-shell-type (quote ("shell" "*shell*" (lambda nil (shell)))))
  '(shell-pop-universal-key "M-`")
  '(shell-pop-windown-position "bottom")
