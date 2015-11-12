7523e5175e96328f2b87db775a698dd61fe9fb8b
Gabriel Chavez <gabriel@chavezgu.com>
2015-11-12 14:35:59 +0200

    Add Steve Yegge advice remaps.

--- init.el
+++ init.el
@@ -294,3 +294,12 @@
              '("*magit\-log\:.*". ((display-buffer-use-some-window) .
                                    ((inhibit-same-window . t)))))
 
+;; We follow Steve Yegge advice
+;; No more Alt-x
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+;; No more backspace
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
