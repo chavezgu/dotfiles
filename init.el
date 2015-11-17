35ece08fe96f31356c8eb6b0519ebcb0965f25a7
Gabriel Chavez <gabriel@chavezgu.com>
2015-11-17 09:28:25 +0200

    Expand yasnippet the way it was supposed to be.

--- init.el
+++ init.el
@@ -58,9 +58,9 @@
 ;; (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
-(define-key yas-minor-mode-map (kbd "<tab>") nil)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
-(define-key yas-minor-mode-map (kbd "C-SPC") 'yas-expand)
+(define-key yas-minor-mode-map (kbd "<tab>") 'yas-expand)
+(define-key yas-minor-mode-map (kbd "TAB") 'yas-expand)
+
 
 ;; Use electric pair
 (electric-pair-mode 1)
