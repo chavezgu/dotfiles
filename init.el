3f86dc82a753f0a6a4d66a7798d39ec36eeda5ac
Gabriel Chavez <gabriel.chavez@ericsson.com>
2015-11-18 11:34:45 +0200

    No more verbose commit messages.

--- init.el
+++ init.el
@@ -85,7 +85,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-commit-arguments (quote ("--verbose")))
  '(org-babel-load-languages (quote ((C . t) (emacs-lisp . t))))
  '(shell-pop-shell-type (quote ("shell" "*shell*" (lambda nil (eshell)))))
  '(shell-pop-universal-key "M-`")
