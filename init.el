5259793ca36bebf9d0d785064dd95bd2d7bf5144
Gabriel Chavez <gabriel@chavezgu.com>
2015-11-27 12:31:01 +0200

    No more shell-pop.

--- init.el
+++ init.el
@@ -86,9 +86,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-babel-load-languages (quote ((C . t) (emacs-lisp . t))))
- '(shell-pop-shell-type (quote ("shell" "*shell*" (lambda nil (eshell)))))
- '(shell-pop-universal-key "M-`")
- '(shell-pop-windown-position "bottom")
  '(show-paren-mode t)
  '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
 (custom-set-faces
@@ -266,7 +263,7 @@
   (interactive)
   (other-window -1))
 (global-set-key "\M-O" 'prev-window)
- 
+
 ;; Make man pages open in the same window.
 (setq Man-notify-method 'pushy)
 
