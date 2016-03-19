f0dd84264d99a1cdd213dbcdc935b783a8c6a898
Gabriel Chavez <gabriel@chavezgu.com>
2016-03-19 14:13:18 +0000

    Set company-idle-delay to non-nil.

--- init.el
+++ init.el
@@ -173,7 +173,6 @@
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 (add-to-list 'company-backends 'company-c-headers)
-(setq company-idle-delay nil)
 (global-set-key (kbd "C-<tab>") 'company-complete)
 
 ;; Compile and Run,
