02eaca33593fdca9fc6f84d40f59fe281f1c3a5c
Gabriel Chavez <gabriel@chavezgu.com>
2016-03-23 14:08:32 +0200

    Succinct the commented lines.

--- init.el
+++ init.el
@@ -299,14 +299,9 @@
 
 ;; (require 'cc-mode)
 ;; (require 'semantic)
-
 ;; (global-semanticdb-minor-mode 1)
 ;; (global-semantic-idle-scheduler-mode 1)
-
 ;; (semantic-mode 1)
-
-;; (setq-local eldoc-documentation-function #'ggtags-eldoc-function)
-
 ;; (add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
 
 (require 'tramp)
