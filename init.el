f3cc270f9a0a8385c06c7bf0a1de45ba32da6b3b
Gabriel Chavez <gabriel.chavez@ericsson.com>
2016-05-06 15:10:28 +0300

    function-args.
    Acked-by: Gabriel Chavez <gabriel.chavez@ericsson.com>

--- init.el
+++ init.el
@@ -175,6 +175,9 @@
 (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
 (define-key helm-gtags-mode-map (kbd "M-]") 'helm-gtags-find-rtag)
 
+;; function-args is awesome
+(fa-config-default)
+
 ;; (require 'ggtags)
 ;; (add-hook 'c-mode-common-hook
 ;;           (lambda ()
