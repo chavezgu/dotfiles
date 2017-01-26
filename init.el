10b97ef613cb167d8d0adcbf4a4f297a020d60cf
Gabriel Chavez <gabriel.chavez@ericsson.com>
2017-01-26 17:45:34 +0200

    emacs: Change the yas-expand keybinding.

--- init.el
+++ init.el
@@ -60,8 +60,7 @@
 ;; (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
-(define-key yas-minor-mode-map (kbd "<tab>") 'yas-expand)
-(define-key yas-minor-mode-map (kbd "TAB") 'yas-expand)
+(define-key yas-minor-mode-map (kbd "M-RET") 'yas-expand)
 
 ;; GRB: highlight trailing whitespace
 (set-default 'show-trailing-whitespace t)
