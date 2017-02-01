287245b407af8753682f2e1a7c2e0d344012b7e8
Gabriel Chavez <gabriel.chavez@ericsson.com>
2017-02-01 15:42:05 +0200

    update apps to for certain files.

--- init.el
+++ init.el
@@ -267,11 +267,12 @@
 (add-to-list 'dired-guess-shell-alist-user '("\\.pdf\\'" "mupdf"))
 (add-to-list 'dired-guess-shell-alist-user '("\\.azw3\\'" "ebook-viewer"))
 (add-to-list 'dired-guess-shell-alist-user '("\\.mobi\\'" "ebook-viewer"))
-(add-to-list 'dired-guess-shell-alist-user '("\\.mp4\\'" "totem"))
-(add-to-list 'dired-guess-shell-alist-user '("\\.mkv\\'" "totem"))
-(add-to-list 'dired-guess-shell-alist-user '("\\.png\\'" "eog"))
-(add-to-list 'dired-guess-shell-alist-user '("\\.gif\\'" "eog"))
-(add-to-list 'dired-guess-shell-alist-user '("\\.jpg\\'" "eog"))
+(add-to-list 'dired-guess-shell-alist-user '("\\.epub\\'" "ebook-viewer"))
+(add-to-list 'dired-guess-shell-alist-user '("\\.mp4\\'" "mplayer"))
+(add-to-list 'dired-guess-shell-alist-user '("\\.mkv\\'" "mplayer"))
+(add-to-list 'dired-guess-shell-alist-user '("\\.png\\'" "gpicview"))
+(add-to-list 'dired-guess-shell-alist-user '("\\.gif\\'" "gpicview"))
+(add-to-list 'dired-guess-shell-alist-user '("\\.jpg\\'" "gpicview"))
 
 ;; Sudo in TRAMP
 (defun sudo ()
