c9c55ecddfcf5a8cec183d0bfa342bf5d4d02cba
Gabriel Chavez <gabriel@chavezgu.com>
2017-02-28 08:28:53 +0200

    emacs: Change font size to 130

--- init.el
+++ init.el
@@ -124,7 +124,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 140 :width normal)))))
+ '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 130 :width normal)))))
 
 ;; Some c-mode stuff
 (setq c-default-style "linux")
