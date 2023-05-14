9fa03cf4eb3431d6685afa98b67a8959c195ac50
Gabriel Chavez <gabriel@chavezgu.com>
2023-05-14 09:21:43 +0300

    emacs: Organize sections better
    Try to have each block of related changes closer to each other.

--- init.el
+++ init.el
@@ -1,35 +1,23 @@
-;; ido is great
+;; Requires
+(require 'uniquify)
+
+;; Modes
 (ido-mode t)
-(setq ido-enable-flex-matching t)
-
-;; No bars
 (menu-bar-mode -1)
-
-;; Check if there's a tool mode
+(save-place-mode 1)
+(show-paren-mode 1)
+(savehist-mode 1)
+(electric-pair-mode 1)
+(column-number-mode 1)
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 
+;; Autoloads and aliases
+(defalias 'yes-or-no-p 'y-or-n-p)
 (autoload 'zap-up-to-char "misc"
   "Kill up to, but not including ARGth occurrence of CHAR." t)
 
-;; Shamelessly copied from better-defaults:
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-
-;; https://www.emacswiki.org/emacs/SavePlace
-(save-place-mode 1)
-
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-(global-set-key (kbd "M-z") 'zap-up-to-char)
-
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
-
-(show-paren-mode 1)
-(setq-default indent-tabs-mode nil)
-(savehist-mode 1)
+;; Define variables
 (setq save-interprogram-paste-before-kill t
       apropos-do-all t
       mouse-yank-at-point t
@@ -38,36 +26,39 @@
       load-prefer-newer t
       backup-by-copying t
       frame-inhibit-implied-resize t
+      ido-enable-flex-matching t
+      uniquify-buffer-name-style 'forward
+      indent-tabs-mode nil
       ediff-window-setup-function 'ediff-setup-windows-plain
+      find-file-visit-truename t
       custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 ;; Finnish keyboard things
 (global-set-key (kbd "M-;") 'beginning-of-buffer)
 (global-set-key (kbd "M-ö") 'comment-dwim)
-
 (global-set-key (kbd "M-:") 'end-of-buffer)
 (global-set-key (kbd "M-ä") 'eval-expression)
-
 (global-set-key (kbd "M--") 'hippie-expand)
+(global-set-key (kbd "C--") 'undo)
 
-;; tango-dark is poor man's version of monokai
-(load-theme 'tango-dark)
-
-;; Kill region
+;; Better global keybindings
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "M-z") 'zap-up-to-char)
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
 (global-set-key (kbd "C-x C-k") 'kill-region)
 (global-set-key (kbd "C-c C-k") 'kill-region)
-
-;; One letter please
-(defalias 'yes-or-no-p 'y-or-n-p)
+(global-set-key (kbd "M-o") 'other-window)
 
 ;; org-mode things
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
 
-;; Lispy things
-(electric-pair-mode 1)
 
-;;; Cascadia Font is now the best font
+;; Appearance settings
 (custom-set-faces
  '(default ((t (:family "Cascadia Code" :foundry "unknown" :slant normal :weight normal :height 140 :width normal)))))
+(load-theme 'tango-dark)
