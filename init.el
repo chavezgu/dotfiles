ea9835eeb62e2dcd4a154592ae5d634f058882a3
Gabriel Chavez <chavezgu@outlook.com>
2026-01-05 09:55:53 +0200

    emacs: Use tango light.
    It's easier to certain things with it than with tango dark.

--- init.el
+++ init.el
@@ -98,7 +98,7 @@
 (global-set-key (kbd "M-å") 'my-switch-windows)
 
 ;; Tango dark is decent and it's part of emacs
-(load-theme 'tango-dark)
+(load-theme 'tango)
 ;; Melpa stuff
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
