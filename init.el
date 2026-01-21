41b1675d63b5d520678fa5320ccedbd6f19c30f8
Gabriel Chavez <chavezgu@outlook.com>
2026-01-21 10:17:07 +0200

    emacs: Add org-switchb keyshortcut

--- init.el
+++ init.el
@@ -62,6 +62,7 @@
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
+(global-set-key (kbd "C-c b") #'org-switchb)
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 
 ;; Org-mode spellchecking configuration
