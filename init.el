e1cd1f79146fcc697b83b37c212ed8d2800eea8c
Gabriel Chavez <chavezgu@outlook.com>
2026-02-26 12:53:27 +0200

    emacs: Remove MELPA configuration
    We are going to use Emacs only with default values.

--- init.el
+++ init.el
@@ -92,9 +92,3 @@
 
 ;; Tango dark is decent and it's part of emacs
 (load-theme 'tango-dark)
-
-;; Melpa stuff
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
