e34e7bfb69cc758779d273c058a10cd78ee1f29f
Gabriel Chavez <chavezgu@outlook.com>
2026-08-25 22:03:01 +0300

    Revert "emacs: Remove font configuration"
    This reverts commit 4dcf00b630534422040c34183a01cf4d212ab6ed.

--- init.el
+++ init.el
@@ -69,6 +69,10 @@
 (setq ispell-program-name "enchant-2")
 (setq ispell-dictionary "fi")
 
+;; Let's have this back:
+(custom-set-faces
+ '(default ((t (:family "CaskaydiaMono Nerd Font Mono" :foundry "unknown" :slant normal :weight normal :height 140 :width normal)))))
+
 ;; Some weird compilation thing, that should be part of emacs
 (defun my-ansi-colorize-buffer ()
   (let ((buffer-read-only nil))
